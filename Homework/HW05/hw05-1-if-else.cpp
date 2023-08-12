f9daf48188fc7dbd0d2ae096e2f05f45dc6fa373
Mekha1104 <gtrcooie@gmail.com>
2023-08-12 14:43:06 +0700

    05-1

--- Homework/HW05/hw05-1-if-else.cpp
+++ Homework/HW05/hw05-1-if-else.cpp
@@ -15,19 +15,110 @@
 #include <string>
 
 int main() {
-    std::cout << "Please enter characters:\n";
-    std::string input;
-    std::cin >> input;
+    char input1, input2, input3, input4;
 
-    if (input.length() != 4) {
-        std::cout << "Please enter characters\n";
-        return 1;
-    }
+    std::cout << "Please enter characters: ";
+    std::cin >> input1 >> input2 >> input3 >> input4;
 
     std::cout << "Result: ";
-    for (int i = input.length() - 1; i >= 0; i--) {
-        std::cout << input[i] << " ";
+
+    if (input1 >= input2 && input1 >= input3 && input1 >= input4) {
+        std::cout << input1 << " ";
+        if (input2 >= input3 && input2 >= input4) {
+            std::cout << input2 << " ";
+            if (input3 >= input4) {
+                std::cout << input3 << " " << input4;
+            } else {
+                std::cout << input4 << " " << input3;
+            }
+        } else if (input3 >= input2 && input3 >= input4) {
+            std::cout << input3 << " ";
+            if (input2 >= input4) {
+                std::cout << input2 << " " << input4;
+            } else {
+                std::cout << input4 << " " << input2;
+            }
+        } else {
+            std::cout << input4 << " ";
+            if (input2 >= input3) {
+                std::cout << input2 << " " << input3;
+            } else {
+                std::cout << input3 << " " << input2;
+            }
+        }
+    } else if (input2 >= input1 && input2 >= input3 && input2 >= input4) {
+        std::cout << input2 << " ";
+        if (input1 >= input3 && input1 >= input4) {
+            std::cout << input1 << " ";
+            if (input3 >= input4) {
+                std::cout << input3 << " " << input4;
+            } else {
+                std::cout << input4 << " " << input3;
+            }
+        } else if (input3 >= input1 && input3 >= input4) {
+            std::cout << input3 << " ";
+            if (input1 >= input4) {
+                std::cout << input1 << " " << input4;
+            } else {
+                std::cout << input4 << " " << input1;
+            }
+        } else {
+            std::cout << input4 << " ";
+            if (input1 >= input3) {
+                std::cout << input1 << " " << input3;
+            } else {
+                std::cout << input3 << " " << input1;
+            }
+        }
+    } else if (input3 >= input1 && input3 >= input2 && input3 >= input4) {
+        std::cout << input3 << " ";
+        if (input1 >= input2 && input1 >= input4) {
+            std::cout << input1 << " ";
+            if (input2 >= input4) {
+                std::cout << input2 << " " << input4;
+            } else {
+                std::cout << input4 << " " << input2;
+            }
+        } else if (input2 >= input1 && input2 >= input4) {
+            std::cout << input2 << " ";
+            if (input1 >= input4) {
+                std::cout << input1 << " " << input4;
+            } else {
+                std::cout << input4 << " " << input1;
+            }
+        } else {
+            std::cout << input4 << " ";
+            if (input1 >= input2) {
+                std::cout << input1 << " " << input2;
+            } else {
+                std::cout << input2 << " " << input1;
+            }
+        }
+    } else {
+        std::cout << input4 << " ";
+        if (input1 >= input2 && input1 >= input3) {
+            std::cout << input1 << " ";
+            if (input2 >= input3) {
+                std::cout << input2 << " " << input3;
+            } else {
+                std::cout << input3 << " " << input2;
+            }
+        } else if (input2 >= input1 && input2 >= input3) {
+            std::cout << input2 << " ";
+            if (input1 >= input3) {
+                std::cout << input1 << " " << input3;
+            } else {
+                std::cout << input3 << " " << input1;
+            }
+        } else {
+            std::cout << input3 << " ";
+            if (input1 >= input2) {
+                std::cout << input1 << " " << input2;
+            } else {
+                std::cout << input2 << " " << input1;
+            }
+        }
     }
-    
+
     return 0;
 }
